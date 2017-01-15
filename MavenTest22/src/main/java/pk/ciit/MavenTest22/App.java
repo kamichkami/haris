206bb72defed05c121a227ec74a2409cc0e1d998
kamran akhtar <kamran.kami161@gmail.com>
2017-01-15 17:53:10 +0500

    ye andar

---
+++ MavenTest22/src/main/java/pk/ciit/MavenTest22/App.java
@@ -0,0 +1,13 @@
+package pk.ciit.MavenTest22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
